393185cfb60e231e1cf0235e58ea16645fc4de1c
George Lucas Lazarim <gll@asusf15ubuntu.tailaac83f.ts.net>
2026-04-20 15:43:07 -0300

    firstSimpleQueueCommit

---
+++ simplequeue.c
@@ -0,0 +1,18 @@
+#include "queue.h"
+#include <time.h>
+
+int main() {
+    int n;
+
+    struct queue* queue = createQueue();
+
+    srand(time(NULL));
+
+    printf("\nEnter the number of random elements you want to insert into the queue: ");
+    scanf("%d", &n);
+
+    for (int i=0; i < n; i++) enqueue(queue, rand() % 1000);
+    
+    if (!isEmpty(queue)) printf("\nRemoving the front element from the list (the first element to enter the list): %d", dequeue(queue));
+
+}
