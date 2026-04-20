ed2ffe676e20775f8a6a692ab08cb75f83d062df
George Lucas Lazarim <gll@asusf15ubuntu.tailaac83f.ts.net>
2026-04-20 16:51:02 -0300

    implementingThePrintDeleteAndPeekFunctions

--- simplequeue.c
+++ simplequeue.c
@@ -13,6 +13,13 @@ int main() {
 
     for (int i=0; i < n; i++) enqueue(queue, rand() % 1000);
 
-    if (!isEmpty(queue)) printf("\nRemoving the front element from the list (the first element to enter the list): %d\n", dequeue(queue));
+    printf("\nPrinting the random list: ");
+    printQueue(queue);
 
+    printf("\nRemoving the front element from the list (the last element to enter the list): %d", dequeue(queue));
+    printf("\nPeeking at the front element of the random list: %d", peek(queue));
+
+    printf("\nDeleting the random list... ");
+    deleteQueue(&queue);
+    printf("Deleted!\n");
 }
