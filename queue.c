37c885845e9a62be6aa7933c5dd05f9803dae4a7
George Lucas Lazarim <gll@asusf15ubuntu.tailaac83f.ts.net>
2026-04-20 15:23:56 -0300

    DebugFunctions

--- queue.c
+++ queue.c
@@ -3,22 +3,27 @@
 struct queue* createQueue() {
     struct queue* queue = (struct queue*) malloc (sizeof(struct queue));
     if (queue == NULL) return queue;
-    queue->front = queue->rear = NULL;
 
+    queue->front = queue->rear = NULL;
     return queue;
 }
 
 bool isEmpty (struct queue* queue) {
-    if (queue->front == NULL) return true;
-    else return false;
+    return (queue->front == NULL);
 }
 
 void enqueue (struct queue* queue, int data) {
     struct queueNode* newNode = (struct queueNode*) malloc (sizeof(struct queueNode));
+
+    if (newNode == NULL) {
+        printf("Error! Memory allocation failure");
+        return;
+    }
+
     newNode->data = data;
+    newNode->next = NULL;
 
     if (queue->front == NULL) {
-        newNode->next = NULL;
         queue->front = queue->rear = newNode;
     } else {
         queue->rear->next = newNode;
@@ -29,13 +34,15 @@ void enqueue (struct queue* queue, int data) {
 int dequeue (struct queue* queue) {
     if (queue->front == NULL) {
         printf("Error! The Queue is Empty");
-        
         return -1;
     } else {
         struct queueNode* tempNode = queue->front;
         int data = tempNode->data;
 
         queue->front = queue->front->next;
+
+        if (queue->front == NULL) queue->rear = NULL;
+        
         free(tempNode);
 
         return data;
