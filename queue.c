dd0007b653cde62912e647f3d3c78b8a24fd7592
George Lucas Lazarim <gll@asusf15ubuntu.tailaac83f.ts.net>
2026-04-20 14:32:44 -0300

    createQueueFunction

--- queue.c
+++ queue.c
@@ -1,2 +1,8 @@
 #include "queue.h"
 
+struct queue* createQueue() {
+    struct queue* queue = (struct queue*) malloc (sizeof(struct queue));
+    if (queue == NULL) return NULL;
+    queue->front = queue->rear = NULL;
+    return NULL;
+}
--- queue.h
+++ queue.h
@@ -1,6 +1,17 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+struct queueNode {
+    int data;
+    struct queueNode* next;
+};
+
+struct queue {
+    struct queueNode* front;
+    struct queueNode* rear;
+};
 
 #endif
